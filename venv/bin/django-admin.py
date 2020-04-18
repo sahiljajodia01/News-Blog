4d60d023a47178ffde72d69c78d6349561b9da4a
Sahil Jajodia <sahil.jajodia@gmail.com>
2020-04-18 19:08:16 +0530

    Change pillow version

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/sahiljajodia/Projects/DjangoProjects/test-blog/venv/bin/python3.6
+#!/Users/sahiljajodia/Github/News-Blog/venv/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
